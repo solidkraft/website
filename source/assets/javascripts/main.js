14216384cf59f423d1e45201e2073c26d627e59c
Jessica <jess.tasstic@gmail.com>
2020-08-01 00:38:28 -0400

    Update main.js

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -5,3 +5,29 @@ $(document).ready(function() {
     $(this).addClass('active');
   })
 })
+
+$(document).ready(main);
+var counter = 1;
+function main(){
+  $('.fixed-header').click(function(){
+		if(counter == 1){
+      $('.mobile-menu').animate({
+        right: '0'
+      });
+      counter = 0;
+    } else {
+      counter = 1;
+      $('.mobile-menu').animate({
+        right: '-100%'
+      });
+    }
+	});
+};
+$(document).ready(function() {
+  $('.mobile-menu ol li').click(function(){
+    $('.mobile-menu').animate({
+      right: '-100%'
+    });
+    counter = 1;
+  })
+})
